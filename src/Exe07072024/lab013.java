5b8ae02e08b72ae378b3e48dffac849d20207236
java c <bbtdgr420@gmail.com>
2024-07-28 23:57:22 +0530

    All files added

---
+++ src/Exe07072024/lab013.java
@@ -0,0 +1,13 @@
+package Exe07072024;
+
+public class lab013 {
+
+// can i run  class without main method
+// run option. no run option without main function
+//  now i create two main function
+public static void main(String[] args) {
+    // jvm will recognise only above  main function
+}
+public static void main()
+{
+}}
---
+++ src/Exe07072024/lab014.java
@@ -0,0 +1,11 @@
+package Exe07072024;
+
+public class lab014 {
+    //public static void main(String[] args)
+    public static void main(String args[])
+    //both are same
+    {
+        System.out.println("yes");
+
+    }
+}
